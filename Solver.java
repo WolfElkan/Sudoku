c0e00a06a2d4115a9e50a0e7fbd6903621e85ce6
Wolf Elkan <wolf.elkan@gmail.com>
2020-03-14 00:18:51 -0400

    basic inheritance

--- Solver.java
+++ Solver.java
@@ -1,1 +1,27 @@
-solve.win;
+abstract class Sudoku {
+	int content;
+	public Sudoku(int size) {
+		content = size;
+	}
+	public abstract void print();
+}
+
+class Sudoku3x3 extends Sudoku {
+	public Sudoku3x3() {
+		super(81);
+	}
+	public void print() {
+		System.out.println(content);
+		// for (int i=0; i<size; i++) {
+		// 	System.out.print(content[i]);
+		// }
+		// System.out.println();
+	}
+}
+
+class Solver {
+	public static void main(String[] args) {
+		Sudoku3x3 sd = new Sudoku3x3();
+		sd.print();
+	}
+}
