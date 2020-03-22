08b0731fb2fa716699e13e5860f68716cd81f511
Wolf Elkan <wolf.elkan@gmail.com>
2020-03-21 22:21:11 -0400

    Extended print method for puzzles of any size

--- Solver.java
+++ Solver.java
@@ -1,43 +1,63 @@
 abstract class Sudoku {
+	public int pow(int num, int exp) {
+		int result = 1;
+		for (int i=0; i<exp; i++) {
+			result *= num;
+		}
+		return result;
+	}
 	int content[];
 	int size;
-	public Sudoku(int size) {
-		this.size = size;
+	int scale;
+	public Sudoku(int scale) {
+		this.scale = scale;
+		this.size = pow(scale,4);
 		content = new int[size];
 	}
-	public abstract void print();
-}
-
-class Sudoku3x3 extends Sudoku {
-	public Sudoku3x3() {
-		super(81);
-	}
 	public void print() {
-		String bar = "+-------+-------+-------+";
-		System.out.println(bar);
-		System.out.print("| ");
+		String bar = "+"; // Construct bar
+		for (int i=0; i<scale; i++) {
+			for (int j=0; j < scale * 2 + 1; j++) {
+				bar += "-";
+			}
+			bar += "+";
+		}
+		System.out.println(bar); // Print top bar
+		System.out.print("| "); // Print left bar
 		for (int i=0; i<size; i++) {
-			System.out.print(content[i]);
-			System.out.print(' ');
-			if (i % 27 == 26) {
+			System.out.print(content[i]); // Print number
+			System.out.print(' '); // print space
+			if (i % pow(scale,3) == pow(scale,3)-1) { // Print mid bar
 				System.out.print("| ");
 				System.out.println();
 				System.out.println(bar);
 				if (i < size - 1) {
 					System.out.print("| ");
 				}
-			} else if (i % 9 == 8) {
+			} else if (i % pow(scale,2) == pow(scale,2)-1) {
 				System.out.print("|\n| ");
-			} else if (i % 3 == 2) {
+			} else if (i % scale == scale - 1) {
 				System.out.print("| ");
 			}
 		}
 	}
 }
 
+class Sudoku3x3 extends Sudoku {
+	public Sudoku3x3() {
+		super(3);
+	}
+}
+
+class Sudoku2x2 extends Sudoku {
+	public Sudoku2x2() {
+		super(2);
+	}
+}
+
 class Solver {
 	public static void main(String[] args) {
-		Sudoku3x3 sd = new Sudoku3x3();
+		Sudoku2x2 sd = new Sudoku2x2();
 		sd.print();
 	}
 }
